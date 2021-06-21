203f84f3203700007a949ef62f6f9c543afb90b2
Fahim1556 <fahimhasanthebest@gmail.com>
2021-06-21 12:02:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import DataTable from "./components/DataTable";
 function App() {
+  const [data, setData] = useState([
+
+    {
+      moduleName: "Configuration",
+      activityName: "Item Category",
+      isCreate: false,
+      isView: false,
+      isEdit: false,
+      isDelete: false,
+      isApprove: false,
+    },
+    {
+      moduleName: "Configuration",
+      activityName: "Item",
+      isCreate: false,
+      isView: false,
+      isEdit: false,
+      isDelete: false,
+      isApprove: false,
+    },
+    {
+      moduleName: "Configuration",
+      activityName: "Party",
+      isCreate: false,
+      isView: false,
+      isEdit: false,
+      isDelete: false,
+      isApprove: false,
+    },
+    {
+      moduleName: "Configuration",
+      activityName: "Profile",
+      isCreate: false,
+      isView: false,
+      isEdit: false,
+      isDelete: false,
+      isApprove: false,
+    },
+    {
+      moduleName: "Configuration",
+      activityName: "Bank Account",
+      isCreate: false,
+      isView: false,
+      isEdit: false,
+      isDelete: false,
+      isApprove: false,
+    },
+    {
+      moduleName: "Configuration",
+      activityName: "Change Password",
+      isCreate: false,
+      isView: false,
+      isEdit: false,
+      isDelete: false,
+      isApprove: false,
+    },
+    {
+      moduleName: "Purchase",
+      activityName: "Purchase",
+      isCreate: false,
+      isView: false,
+      isEdit: false,
+      isDelete: false,
+      isApprove: false,
+    },
+
+    {
+      moduleName: "Purchase",
+      activityName: "Due Payment",
+      isCreate: false,
+      isView: false,
+      isEdit: false,
+      isDelete: false,
+      isApprove: false,
+    },
+
+    {
+      moduleName: "Sales",
+      activityName: "Sales",
+      isCreate: false,
+      isView: false,
+      isEdit: false,
+      isDelete: false,
+      isApprove: false,
+    },
+
+    {
+      moduleName: "Sales",
+      activityName: "Due Receive",
+      isCreate: false,
+      isView: false,
+      isEdit: false,
+      isDelete: false,
+      isApprove: false,
+    },
+
+    {
+      moduleName: "Accounts",
+      activityName: "Voucher",
+      isCreate: false,
+      isView: false,
+      isEdit: false,
+      isDelete: false,
+      isApprove: false,
+    },
+  ]);
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataTable data={data} setData={setData} />
     </div>
   );
 }
